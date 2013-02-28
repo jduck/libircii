e679200ec71f720c271a7fccf47a586c70908a54
Joshua J. Drake <github.jdrake@qoop.org>
2013-02-28 13:12:44 -0600

    Correct format string vuln!

--- libircii.c
+++ libircii.c
@@ -299,11 +299,11 @@ ircii_print(ircII i)
    memset(tb2, 0, sizeof(tb2));
    if (i.user && i.host)
      uh = 1;
-   snprintf(tb2, sizeof(tb2)-1, "%-12.12s%s%s%s%s%s%s%s%s%s%s%s\n", tb,
+   snprintf(tb2, sizeof(tb2)-1, "%-12.12s%s%s%s%s%s%s%s%s%s%s%s", tb,
 	    i.from ? " from " : "", i.from ? i.from : "",
 	    uh ? " (" : "", uh ? i.user : "", uh ? "@" : "", uh ? i.host : "", uh ? ")" : "",
 	    i.targ ? " to " : "", i.targ ? i.targ : "",
 	    i.msg ? ": " : "", i.msg ? i.msg : "");
-   printf(tb2);
+   puts(tb2);
 }
 #endif
